7a33f98b44be8498706b2bc3cb4afa7ef8e3289a
thsejrwn <thsejrwn7808@gmail.com>
2026-03-24 10:44:37 +0900

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Hello World k8s!';
+    return 'Hello World k8s 01!';
   }
 }
